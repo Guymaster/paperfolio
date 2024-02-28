ff9cfc878f66472f09e518c03cab9df757309176
Guymaster <guymaster25@gmail.com>
2024-02-28 21:08:15 +0000

    Setting up general structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
+import './fonts/poppins.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className='bgCard'>OO</div>
+      <div className='bgCard'></div>
       <div className='bgText'>TT</div>
       <div className='infoBox'>
-        lorem ipsum dolor para ...
+        <div className='socialBox'>A</div>
+        <div className='name'>
+          Guy R.
+          <strong>THEA</strong>
+        </div>
+        <div className='jobBox'>
+          <div className='jobInfo'>
+            <div className='jobTitle'>Fullstack Developer</div>
+            <div className='jobDesc'>I implement innovative seee tete </div>
+          </div>
+          <a className='resumeLink' href='https://google.com'>My Resume</a>
+        </div>
       </div>
       <div className='pagesBox'>
         <div className='page'></div>
