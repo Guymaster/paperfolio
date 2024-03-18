2ecdc69fb4282b070c73c49f94e0951c1fedc1a7
Guymaster <guymaster25@gmail.com>
2024-03-18 21:10:57 +0000

    Setting up bg movement

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './fonts/poppins.css';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
 
 function App() {
+  const [bgLeft, setBgLeft] = useState(0);
+
+  const bgRef = useRef(null);
+
+  const handleScroll = (e) => {
+    setBgLeft(-3.4*(bgRef.current.offsetWidth*window.scrollY)/(document.documentElement.scrollHeight - window.screen.height))
+  };
+  const handleGoToTop = (e) => {
+    window.scrollTo(0, 0)
+  };
+
+  useEffect(()=>{
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="App">
       <div className='bgCard'></div>
-      <div className='bgText'>
-        <div>GUYMASTER</div>
+      <div className='bgText' ref={bgRef}>
+        <div style={{position: "fixed", left: bgLeft}}>GUYMASTER</div>
       </div>
       <div className='infoBox'>
         <div className='socialBox'>
           <div className='contactMe'>Contact Me</div>
           <a className='linkedinBTN' href='https://https://www.linkedin.com/in/guy-roger-thea'>
             <svg>
-                <g stroke="none" stroke-width="1" fill-rule="evenodd">
+                <g stroke="none">
                     <g id="Dribbble-Light-Preview" transform="translate(-180.000000, -7479.000000)">
                         <g transform="translate(56.000000, 160.000000)">
                             <path d="M144,7339 L140,7339 L140,7332.001 C140,7330.081 139.153,7329.01 137.634,7329.01 C135.981,7329.01 135,7330.126 135,7332.001 L135,7339 L131,7339 L131,7326 L135,7326 L135,7327.462 C135,7327.462 136.255,7325.26 139.083,7325.26 C141.912,7325.26 144,7326.986 144,7330.558 L144,7339 L144,7339 Z M126.442,7323.921 C125.093,7323.921 124,7322.819 124,7321.46 C124,7320.102 125.093,7319 126.442,7319 C127.79,7319 128.883,7320.102 128.883,7321.46 C128.884,7322.819 127.79,7323.921 126.442,7323.921 L126.442,7323.921 Z M124,7339 L129,7339 L129,7326 L124,7326 L124,7339 Z" id="linkedin-[#161]">
@@ -39,7 +57,7 @@ function App() {
             <div className='jobTitle'>Fullstack Developer</div>
             <div className='jobDesc'>I imagine and develop innovative software solutions for web and mobile platforms since 2020</div>
           </div>
-          <a className='resumeLink' href='https://google.com'>My Resume</a>
+          <a className='resumeLink' href='https://drive.google.com/file/d/1eucfMDBZGcfdMjtwalWEGzrozDoyJt51/view?usp=drive_link'>My Resume</a>
         </div>
       </div>
       <div className='pagesBox'>
@@ -51,7 +69,7 @@ function App() {
           </p>
           <p>
             <strong>Web App Development : </strong>
-            From static landing pages and home pages to huge web apps including complex features and lifecycle
+            From static landing pages and home pages to huge web apps including complex features and lifecycles
           </p>
           <p>
             <strong>Mobile App Development : </strong>
@@ -141,7 +159,9 @@ function App() {
             <br/>
           </p>
         </div>
+        <div className='page'></div>
       </div>
+      <div onClick={handleGoToTop} className={bgLeft < -500? 'goToTop visible' : 'goToTop'}>Go to Top</div>
     </div>
   );
 }
